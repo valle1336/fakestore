a157f8e92bbb0a048b64af6e68e26e7ffe230ce1
valle1336 <112933517+valle1336@users.noreply.github.com>
2023-11-10 16:18:52 +0100

    sfdhdfsh

---
+++ components/navbar.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from 'next'
+
+interface Props {}
+
+const Navbar: NextPage<Props> = ({}) => {
+  return <div></div>
+}
+
+export default Navbar
---
+++ fakestore/pages/api/getData.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from 'next'
+
+interface Props {}
+
+
+
+const GetData: NextPage<Props> = ({}) => {
+
+
+  return <div></div>
+}
+
+export default GetData
